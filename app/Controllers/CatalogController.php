307a70ca2323760f156e9cfacfb9a5a35a6147b8
Anastasia S <martletanastasia@gmail.com>
2021-11-21 19:24:37 +0300

    feat: multi files uploading

--- app/Controllers/CatalogController.php
+++ app/Controllers/CatalogController.php
@@ -3,6 +3,7 @@
     namespace App\Controllers;
 
     use App\Models\Product;
+    define("MB", 1048576);
 
     class CatalogController
     {
@@ -24,8 +25,80 @@
             render('addProductForm.php');
         }
 
-        public function saveProduct() {
-            $path = $_SERVER['DOCUMENT_ROOT'].'/'.$_FILES['img']['name'];
-            move_uploaded_file($_FILES['img']['tmp_name'], $path);
+        public function showMultiForm() {
+            render('addProductMultiForm.php');
         }
+
+        public function getFileInfo($index, $value) {
+
+            if($value == 'type') {
+                $fileInfo = mime_content_type($_FILES['img']['tmp_name'][$index]);
+            } else {
+                $fileInfo = $_FILES['img'][$value][$index];
+            }
+            $fileInfo = explode('/', $fileInfo);
+
+            return $fileInfo[0];
+        }
+
+        public function uniqueFileName($name) {
+            $existFiles = scandir($_SERVER['DOCUMENT_ROOT'].'/downloads/');
+            $i = 0;
+
+            $info = pathinfo($name);
+            $ext = $info['extension'];
+            $filename = $info['filename'];
+
+            while(in_array($name, $existFiles)) {
+                $name = $filename . '_' . $i . '.' . $ext;
+                $i = $i + 1;
+            }
+            return $name;
+        }
+
+        function snackCaseToCamelCase($name) {
+            $updName = ucwords($name, '_');
+            $updName = str_replace('_', '', $updName);
+
+            return lcfirst($updName);
+        }
+
+        public function saveProduct() {
+            $allowedType = 'image'; // NULL to upload all
+            $allowedSize = 1*MB; // -1 to upload all
+            $errors = [];
+            $files = $_FILES['img'];
+
+            for($i = 1; $i <= count($files['name']); $i++) {
+                $index = $i - 1;
+
+                $fileType = $this->getFileInfo($index, 'type');
+                $fileSize = (int) $this->getFileInfo($index, 'size');
+                $fileName = $this->getFileInfo($index, 'name');
+                $fileTmp = $this->getFileInfo($index, 'tmp_name');
+
+                if(is_null($allowedType) || $fileType == $allowedType) {
+                    if($allowedSize == -1 || $fileSize < $allowedSize) {
+                        // snack case to camel case
+                        if(str_contains($fileName, '_')) {
+                            $fileName = $this->snackCaseToCamelCase($fileName);
+                        }
+                        // generate unique file name if needed
+                        $fileName = $this->uniqueFileName($fileName);
+                        $path = $_SERVER['DOCUMENT_ROOT'].'/downloads/'.$fileName;
+                        move_uploaded_file($fileTmp, $path);
+                    } else {
+                        $errors[] = "Error: File <i>" . $fileName . "</i> was not uploaded. Max file size is <i>" . $allowedSize . "</i> \n";
+                    }
+                } else {
+                    $errors[] = "Error: File <i>" . $fileName . "</i> was not uploaded. Only files of type <i>" . $allowedType . "</i> are supported " . "\n";
+                }
+            }
+
+            render('saveProduct.php', [
+                    'errors' => $errors,
+                ]);
+        }
+
+
     }
--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
        '/catalog' => 'App\\Controllers\\CatalogController@index',
        '/product' => 'App\\Controllers\\CatalogController@showProduct',
        '/add_product_form' => 'App\\Controllers\\CatalogController@showForm',
+       '/add_multi_product_form' => 'App\\Controllers\\CatalogController@showMultiForm',
        '/save_product' => 'App\\Controllers\\CatalogController@saveProduct',
    ];
 
